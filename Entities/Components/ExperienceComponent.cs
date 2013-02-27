8232d709143a936ec0a67accbc63b126107badbb
Chris Köbke <c.koebke@hotmail.de>
2013-02-27 01:40:59 +0100

    Removed 1000 given skill points for debugging purposes.

--- Entities/Components/ExperienceComponent.cs
+++ Entities/Components/ExperienceComponent.cs
@@ -16,8 +16,6 @@ namespace Xemio.PrincessDefense.Entities.Components
         /// <param name="entity">The entity.</param>
         public ExperienceComponent(Entity entity) : base(entity)
         {
-            //TODO: only for debugging purposes
-            this.SkillPoints = 1000;
         }
         /// <summary>
         /// Initializes a new instance of the <see cref="ExperienceComponent"/> class.
